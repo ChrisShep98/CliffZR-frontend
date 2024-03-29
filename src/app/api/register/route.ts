62f4b7922b71e60da844b9f333165aa8b402ea8f
ChrisShep98 <shepardchristopher1@gmail.com>
2024-03-28 22:51:00 -0400

    updated register api with postgres db and deleted user route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,28 +1,54 @@
-import { connectMongoDB } from "@/app/lib/mongodb";
-import User from "@/app/models/Users";
+import { db } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(req: any) {
+export async function POST(req: Request) {
   try {
-    const { username, email, password } = await req.json();
-    // try out below code when getting req param to NextApiRequest type
-    // const { userName, email, password } = await req.body.json()
+    const body = await req.json();
+    const { email, username, password } = body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    await connectMongoDB();
 
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return new NextResponse("Email is already in use", { status: 400 });
+    // check if email exists
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "user with this email already exists",
+        },
+        { status: 400 }
+      );
     }
 
-    await User.create({ username, email, password: hashedPassword });
+    // check if username exists
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "user with this username already exists",
+        },
+        { status: 400 }
+      );
+    }
 
-    return NextResponse.json({ message: "User registered" }, { status: 201 });
+    const newUser = await db.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    return NextResponse.json(
+      { user: newUser, message: "User created successfully" },
+      { status: 201 }
+    );
   } catch (error) {
-    return NextResponse.json({ message: "An error occurred while registering" }, { status: 500 });
+    return NextResponse.json({ message: "Something went wrong :(" }, { status: 500 });
   }
 }
