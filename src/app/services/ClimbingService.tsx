a6e1cc63fb44762b9f1e9ac20d317f3af1894c64
ChrisShep98 <shepardchristopher1@gmail.com>
2024-04-03 22:24:19 -0400

    changed api server variable name and string

--- src/app/services/ClimbingService.tsx
+++ src/app/services/ClimbingService.tsx
@@ -1,15 +1,15 @@
-const API_URL = "http://localhost:8080/contacts";
+const LOCAL_API_SERVER = "http://localhost:8080";
 
-export async function saveContact(contact: any) {
-  const response = await fetch(API_URL, {
-    method: "POST",
-    body: JSON.stringify(contact),
-  });
-  return response.json();
-}
+// export async function saveContact(contact: any) {
+//   const response = await fetch(API_URL, {
+//     method: "POST",
+//     body: JSON.stringify(contact),
+//   });
+//   return response.json();
+// }
 
 export async function getUserBoulderingGrades(username: string) {
-  const response = await fetch(`http://localhost:8080/users/${username}`);
+  const response = await fetch(`${LOCAL_API_SERVER}/users/${username}`);
   const data = await response.json();
   return data;
 }
